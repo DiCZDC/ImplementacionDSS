cdb6ee28c0c8b41faddfe381cfdbd9eb284f5a37
DICZDC <zaito.zd@gmail.com>
2026-02-20 23:22:29 -0600

    Agregado un generador de PDF´s

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.input_handler import get_user_input
 from services.solver_service import solve_model
 from utils.output_formatter import show_results
+from utils.pdf.generate_pdf import generate_pdf
 
 def main():
     print("=== DSS Modelo MBI de programación por metas ===\n")
@@ -8,6 +9,7 @@ def main():
     data = get_user_input()
     results = solve_model(data)
     show_results(results)
+    generate_pdf(results)
 
 if __name__ == "__main__":
     main()
